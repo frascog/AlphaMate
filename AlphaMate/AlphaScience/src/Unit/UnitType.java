86fa2394b8b39fe3119b710471d938c60d9f9e93
frascog <frascog@WITR9ZWCKF.wit.private>
2014-12-20 19:08:16 -0500

    Conversion Factors

---
+++ AlphaMate/AlphaScience/src/Unit/UnitType.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Unit;
+
+/**
+ *
+ * @author frascog
+ */
+public enum UnitType {
+    Acceleration,
+    Amount_Of_Substance,
+    Angle,
+    Angular_Acceleration,
+    Angular_Velocity,
+    Area,
+    Blood_Suger,
+    Data_Amount,
+    Data_Rate,
+    Density,
+    Dimensionless,
+    Duration,
+    Dynamic_Viscosity,
+    Electric_Capacitance,
+    ElectricCharge,
+    ElectricConductance,
+    ElectricCurrent,
+    ElectricInductance,
+    ElectricPotential,
+    ElectricResistance,
+    Energy,
+    Force,
+    Frequency,
+    Illuminance,
+    KinematicViscosity,
+    Length,
+    LuminousFlux,
+    LuminousIntensity,
+    MagneticFlux,
+    MagneticFluxDensity,
+    Mass,
+    MassFlowRate,
+    Power,
+    Pressure,
+    RadiationDoseAbsorbed,
+    RadiationDoseEffective,
+    RadioactiveActivity,
+    SolidAngle,
+    Temperature,
+    Torque,
+    Velocity,
+    Volume,
+    VolumetricDensity,
+    VolumetricFlowRate,
+}
