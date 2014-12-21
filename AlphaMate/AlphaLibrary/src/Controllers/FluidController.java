6a8270120829904e30aa20e26b05a525637df072
frascog <frascog@WITR9ZWCKF.wit.private>
2014-12-20 19:11:41 -0500

    New Unit Convertor

--- AlphaMate/AlphaLibrary/src/Controllers/FluidController.java
+++ AlphaMate/AlphaLibrary/src/Controllers/FluidController.java
@@ -193,7 +193,6 @@ public class FluidController {
     }
     
     public void calcuate() {
-        //d = P x MW / (R x T)
         EMeasureController density = new EMeasureController(new EMeasure("Density", SI.KILOGRAM.divide(SI.CUBIC_METRE.times(0.001))));
         density.setEntity(FluidAnalysist.calculateDesnisty(this));
         eMeasureSetController.getEMeasure("Density").setEntity(density.getEntity());
---
+++ AlphaMate/AlphaScience/src/Unit/Unit.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Unit;
+
+/**
+ *
+ * @author frascog
+ */
+public class Unit {
+    
+    private final String symbol;
+    private final UnitType unitType;
+    private final double converstionFactor;
+
+    public Unit(String symbol, UnitType unitType, double converstionFactor) {
+        this.symbol = symbol;
+        this.unitType = unitType;
+        this.converstionFactor = converstionFactor;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public UnitType getUnitType() {
+        return unitType;
+    }
+
+    public double getConverstionFactor() {
+        return converstionFactor;
+    }
+
+    @Override
+    public String toString() {
+        return this.symbol;
+    }
+    
+    
+    
+}
